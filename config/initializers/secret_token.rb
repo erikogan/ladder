1f4840ef449add887e74f0d688f1a8de2aa9d6bf
Erik Ogan <erik@ogan.net>
2014-01-16 15:04:36 -0800

    Straight off the generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vissicitude::Application.config.secret_key_base = '13e7d83b621088ff5589a4f8c115bf01492a19b0cb37df0f88d187f34873db1f9594fdcbc0522de46c82a3fe90ce6670b9240f62e19e54433879b2e9b06a5fc6'
