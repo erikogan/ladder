f7dcb512062a84845e03f508e3ce9c2e2ce9dc15
Erik Ogan <erik@ogan.net>
2014-01-17 09:50:43 -0800

    Allow configs other than hashes

--- config/initializers/00_config_for.rb
+++ config/initializers/00_config_for.rb
@@ -5,8 +5,8 @@ module Ladder
       @configs                  ||= { }
       @configs[filename.to_sym] ||= begin
         config = YAML.load(ERB.new(Rails.root.join("config", filename).read).result(binding))
-        config = config[Rails.env] if config.has_key?(Rails.env)
-        config.with_indifferent_access
+        config = config[Rails.env] if config.respond_to?(:has_key) && config.has_key?(Rails.env)
+        config.respond_to?(:with_indifferent_access) ? config.with_indifferent_access : config
       end
     end
   end
