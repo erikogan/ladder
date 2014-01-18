90b9b6c6d06da827f4910513b47f61b6e36c3a6d
Erik Ogan <erik@ogan.net>
2014-01-18 15:43:41 -0800

    temporarily remove org redirect

--- app/assets/javascripts/org_redirect.js
+++ app/assets/javascripts/org_redirect.js
@@ -2,6 +2,6 @@
   var parts = host.split('.');
   if (parts[parts.length - 1] !== 'org') {
     parts[parts.length - 1] = 'org'
-    window.location = window.location.protocol + '//' + parts.join('.') + '/';
+    // window.location = window.location.protocol + '//' + parts.join('.') + '/';
   }
 }(document.location.hostname));
