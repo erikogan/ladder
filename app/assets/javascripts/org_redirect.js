bbeb01201374e61c71c4f19b23e3a030dad776e9
Erik Ogan <erik@ogan.net>
2014-01-18 14:15:08 -0800

    do not assume the location url has 3 parts in the hostname

--- app/assets/javascripts/org_redirect.js
+++ app/assets/javascripts/org_redirect.js
@@ -1,7 +1,7 @@
 (function (host) {
   var parts = host.split('.');
-  if (parts[2] !== 'org') {
-    parts[2] = 'org'
+  if (parts[parts.length - 1] !== 'org') {
+    parts[parts.length - 1] = 'org'
     window.location = window.location.protocol + '//' + parts.join('.') + '/';
   }
 }(document.location.hostname));
